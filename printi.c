50d573ebab85e1edf139896f2b897ce30468cfe5
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-04-26 05:03:49 +0100

    Finish printi

---
+++ printi.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * printi - prints an integer to stdout
+ * @list: the variable arg list
+ * Return: the number of digits printed
+ */
+int printd(va_list list)
+{	
+	return (printd(list));
+}
